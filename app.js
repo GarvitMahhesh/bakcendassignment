8cd71c1112e5b84818a412096690dc23b0b16e73
Garvit <138558861+GarvitMahhesh@users.noreply.github.com>
2025-01-12 01:12:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swaggerConfig.js';
-
 import connectDB from './Databse/userDB.js';
-connectDB()
+
+// Connect to the database
+connectDB();
 
 const app = express();
+
+// Middleware to parse JSON bodies
 app.use(express.json());
 
+// Serve Swagger UI at the root ("/")
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+// Middleware for parsing URL-encoded data and cookies
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS setup
 app.use(cors());
-app.use(morgan("dev")); 
 
+// Logging middleware
+app.use(morgan("dev"));
 
-
+// API routes prefixed with "/api"
 app.use('/api', userRoutes);
 
-
+// Export the app
 export default app;
